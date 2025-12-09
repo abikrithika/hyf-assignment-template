63692afe6dac7c95c6f0eec787061ccdce693fb9
Abi <abikrithika@gmail.com>
2025-12-09 11:36:19 +0100

    Add query for How many tasks in the task table do not have a valid due date?

--- Database_Assignment/dbQueries.sql
+++ Database_Assignment/dbQueries.sql
@@ -1,3 +1,5 @@
 --How many tasks are in the task table?--
 select COUNT(*) from task
-c
+
+--How many tasks in the task table do not have a valid due date?--
+select COUNT(*) from task where due_date IS NULL
