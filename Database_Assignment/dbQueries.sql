6efa55bda8e61afe651bd14201e8e76fd15fd7f0
Abi <abikrithika@gmail.com>
2025-12-09 11:44:43 +0100

    Add queries for

--- Database_Assignment/dbQueries.sql
+++ Database_Assignment/dbQueries.sql
@@ -3,3 +3,6 @@ select COUNT(*) from task
 
 --How many tasks in the task table do not have a valid due date?--
 select COUNT(*) from task where due_date IS NULL
+
+--Find all the tasks that are marked as done.--
+select title from task where status_id=3
