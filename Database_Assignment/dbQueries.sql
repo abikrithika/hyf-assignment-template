d7df26d329f047905f2c4feaca82950e75d4da90
Abi <abikrithika@gmail.com>
2025-12-10 15:48:57 +0100

    Update queries based on the feedback

--- Database_Assignment/dbQueries.sql
+++ Database_Assignment/dbQueries.sql
@@ -5,10 +5,10 @@ select COUNT(*) from task
 select COUNT(*) from task where due_date IS NULL
 
 --Find all the tasks that are marked as done.--
-select title from task where status_id=3
+select * from task where status_id=3
 
 --Find all the tasks that are not marked as done.--
-select title from task where status_id IN(1,2)
+select title from task where status_id!=3
 
 --Get all the tasks, sorted with the most recently created first.--
 select title from task ORDER BY created DESC
@@ -17,7 +17,7 @@ select title from task ORDER BY created DESC
 select title from task ORDER BY created DESC LIMIT 1
 
 --Get the title and due date of all tasks where the title or description contains database.--
-select title from task where title LIKE '%database%' OR description LIKE '%database%'
+select title,due_date from task where title LIKE '%database%' OR description LIKE '%database%'
 
 --Get the title and status (as text) of all tasks.--
 select task.title, status.name from task JOIN status ON task.status_id=status.id
