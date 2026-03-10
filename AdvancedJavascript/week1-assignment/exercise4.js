ebde5b151f6e171b6ed9d2c0b75bae0fce32b28f
Abi <abikrithika@gmail.com>
2026-03-10 18:34:43 +0100

    feat: implement teasByOrigin to group tea names by origin

---
+++ AdvancedJavascript/week1-assignment/exercise4.js
@@ -0,0 +1,16 @@
+import { teas } from "./teas.js";
+
+function teasByOrigin(teas) {
+  const grouped = {};
+  teas.forEach((tea) => {
+    if (grouped[tea.origin]) {
+      grouped[tea.origin].push(tea.name);
+    } else {
+      grouped[tea.origin] = [tea.name];
+    }
+  });
+
+  return grouped;
+}
+
+console.log(teasByOrigin(teas));
