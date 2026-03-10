f26382d25ca89001fe0a786f53314d26cddc9d7b
Abi <abikrithika@gmail.com>
2026-03-10 20:37:05 +0100

    feat: calculate total tea inventory value using reduce

---
+++ AdvancedJavascript/week1-assignment/exercise6.js
@@ -0,0 +1,7 @@
+import { teas } from "./teas.js";
+
+const totalValue = teas.reduce(
+  (sum, tea) => sum + tea.pricePerGram * tea.stockCount,
+  0,
+);
+console.log("Total inventory value:", totalValue);
