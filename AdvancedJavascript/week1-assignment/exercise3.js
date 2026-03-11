bc195c186c58904264d0c8777d477d64d43b3f2c
Abi <abikrithika@gmail.com>
2026-03-11 17:48:16 +0100

    feat: implement lowStockAlert to return teas with stock below 50 sorted by stock count

--- AdvancedJavascript/week1-assignment/exercise3.js
+++ AdvancedJavascript/week1-assignment/exercise3.js
@@ -1,11 +1,10 @@
 import { teas } from "./teas.js";
 
 function lowStockAlert(teas) {
-  const teaNameAndStockCount = teas.map((tea) => ({
-    name: tea.name,
-    stockCount: tea.stockCount,
-  }));
-  return teaNameAndStockCount.sort((a, b) => a.stockCount - b.stockCount);
+  return teas
+    .filter((tea) => tea.stockCount < 50)
+    .map((tea) => ({ name: tea.name, stockCount: tea.stockCount }))
+    .sort((a, b) => a.stockCount - b.stockCount);
 }
 
 console.log(lowStockAlert(teas));
