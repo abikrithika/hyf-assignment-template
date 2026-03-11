8be1642d2433f26c6163f3ee01c1c5a790fdb368
Abi <abikrithika@gmail.com>
2026-03-11 17:55:21 +0100

    refactor: simplify teasByOrigin by initializing array before pushing

--- AdvancedJavascript/week1-assignment/exercise4.js
+++ AdvancedJavascript/week1-assignment/exercise4.js
@@ -2,12 +2,13 @@ import { teas } from "./teas.js";
 
 function teasByOrigin(teas) {
   const grouped = {};
+
   teas.forEach((tea) => {
-    if (grouped[tea.origin]) {
-      grouped[tea.origin].push(tea.name);
-    } else {
-      grouped[tea.origin] = [tea.name];
+    if (!grouped[tea.origin]) {
+      grouped[tea.origin] = [];
     }
+
+    grouped[tea.origin].push(tea.name);
   });
 
   return grouped;
