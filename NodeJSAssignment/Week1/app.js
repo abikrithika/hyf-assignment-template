9d9ec4fa350fd5186a5918645134718ede8efb6a
Abi <abikrithika@gmail.com>
2026-04-28 20:17:53 +0200

    Add router structure for snippets and tags

--- NodeJSAssignment/Week1/app.js
+++ NodeJSAssignment/Week1/app.js
@@ -1,9 +1,13 @@
 import express from "express";
+import snippetsRouter from "./api/src/routers/snippets.js";
+import tagsRouter from "./api/src/routers/tags.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use("/api/snippets", snippetsRouter);
+app.use("/api/tags", tagsRouter);
 
 app.get("/", (req, res) => {
   res.send("This is a search engine");
