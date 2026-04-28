98f205e6838b19c19ed67019bcd2ab802d2ed33f
Abi <abikrithika@gmail.com>
2026-04-28 20:21:42 +0200

    Initialize in-memory data for snippets and tags

---
+++ NodeJSAssignment/Week1/api/src/routers/snippets.js
@@ -0,0 +1,7 @@
+import express from "express";
+const router = express.Router();
+
+let snippets = [];
+let idCounter = 1;
+
+export default router;
---
+++ NodeJSAssignment/Week1/api/src/routers/tags.js
@@ -0,0 +1,7 @@
+import express from "express";
+const router = express.Router();
+
+let tags = [];
+let tagIdCounter = 1;
+
+export default router;
